55f46072960e18080104ef94c3c0ba3f888a844c
Max Leiter <maxwell_leiter@priorypanther.com>
2015-11-04 18:42:12 -0800

    Filled header

--- fx3dlib.h
+++ fx3dlib.h
@@ -1,15 +1,22 @@
 #ifndef _FX3DLIB_H
 #define _FX3DLIB_H
 #include <knightos/display.h>
-
+#include <stdbool.h>
 #define _FX3DLIB_ID 0x03
 
 #define FX3DLIB_ROTATEVERTEX 6
+unsigned char rotate_vertex(unsigned char locX, unsigned char locY, unsigned char resultX, unsigned char resultY, unsigned char angX, unsigned char angY);
 #define FX3DLIB_PROJECTEVRTEX 9
+unsigned char *project_vertex(unsigned char locX, unsigned char locY);
 #define FX3DLIB_DRAWTRIANGLE 12
+/**TODO: in ASM flip C and B because C = X3 and B = Y3 but the register is BC**/
+unsigned void draw_triangle(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned char x3, unsigned char y3, bool black);
 #define FX3DLIB_MAKEVECTOR 15
+unsigned char make_vector(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned char x3, unsigned char y3);
 #define FX3DLIB_DOTPRODUCT 18
+unsigned char dot_product(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2);
 #define FX3DLIB_CROSSPRODUCT 21
+unsigned char cross_product(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned char x3, unsigned char y3);
 #define FX3DLIB_TESTBACKFACE 24
-
+unsigned char test_backface(unsigned char *face_index, unsigned char *vert_list);
 #endif
